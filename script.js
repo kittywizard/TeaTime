0f44f594517caca73aca5588a6549082a80ea196
marleigh <volcana@gmail.com>
2018-03-02 19:58:22 -0800

    JS Changes~

--- script.js
+++ script.js
@@ -7,19 +7,27 @@ green = document.getElementById('green');
 white = document.getElementById('white');
 black = document.getElementById('black');
 
+var hideButtons = document.getElementById('buttons');
+
 var content = document.querySelector('.content');
 
-
+//CLICK EVENT GREEN TEA
 green.addEventListener("click", function() {
-  //hide all buttons
-  white.classList.toggle("buttonOff");
-  black.classList.toggle("buttonOff");
-  this.classList.toggle("buttonOff");
+  hideEverything();
 
-  //display green Tea
-  //get
+  //display green Tea info
+  var greenDiv = document.createElement('div');
+  greenDiv.innerHTML =
+    "<span class='subHeadline'>Green Tea</span>\
+            <p>Water Temperature: 160&deg; to 180&deg;</p>\
+            <img src='http://placekitten.com/g/300/300' width='300px' height='300px'>";
+            //put backslashes on the innerHTML multiple lines
+
+  //this will add all that innerHTML stuff to the div....
+  document.getElementById('greenTea').appendChild(greenDiv);
 
   //timer
+  //WRITE A FUNCTION NOOB BUG
 
   //reset?
 });
@@ -32,4 +40,9 @@ black.addEventListener("click", function() {
   console.log(black);
 });
 
-//testing
+//function to hide everything -- works on all buttons!
+function hideEverything() {
+  hideButtons.classList.toggle("goAway");
+  //also hide 'choose your type' since its no longer relevant
+  document.getElementById('chooseType').classList.toggle('goAway');
+}
